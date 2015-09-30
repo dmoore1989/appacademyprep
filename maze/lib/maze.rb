d190d1aa9538fede82d8079f4b08ae193218fe72
dmoore1989 <douglas.moore89@gmail.com>
2015-09-29 22:37:04 -0400

    Keep cleaning up maze

--- maze/lib/maze.rb
+++ maze/lib/maze.rb
@@ -1,6 +1,6 @@
 class Maze
   attr_accessor :grid, :position
-  
+
   def initialize(file)
     @file = file
     @grid = File.readlines(file)
@@ -11,30 +11,41 @@ class Maze
     @low_count = nil
     @grid_solution = []
   end
-  
+
   # def [](row, col)
   #   @grid[row][col]
   # end
-  
+
   # def []=(row, col, mark)
   #   @grid[row][col] = mark
   # end
-  
-  def find_solution
-    # debugger
-    if make_move("E")
-      debugger
-        # debugger
-        @low_count = @counter
-        @grid_solution = @grid
-        print_maze
-    end
-    
-    make_move(" ")
 
+  def find_solution
+    # p "find soution"
+    # debugger
+    if move_available?("E")
+      make_move("E")
+      print_maze
+      clear_maze
+    elsif move_available?(" ")
+      make_move(" ")
+    else
+      return
+    end
   end
-  
+
+  def clear_maze
+
+
+  def move_available?(value)
+    @grid[position[0]][position[1] + 1] == value ||
+    @grid[position[0] + 1][position[1]] == value ||
+    @grid[position[0]][position[1] - 1] == value ||
+    @grid[position[0] - 1][position[1]] == value
+  end
+
   def make_move(value)
+    # p "in make move"
     position_store = @position
     grid_store = @grid
     test_position(position[0] + 1, position[1], value)
@@ -45,36 +56,43 @@ class Maze
     @position = position_store
     false
   end
-  
+
   def find_next_move
+    # p "in find next move"
     @grid[position[0]][position[1]] = "x"
-    find_solution 
+    find_solution
   end
-    
+
   def find_start
+    # p "in find start"
     @grid.each_with_index do |line, i|
       return [i, line.index("S")] if line.include?("S")
     end
   end
-  
+
   def print_maze
-    @grid_solution.each do |line|
+    # p "in print maze"
+    # p @grid
+    # p @grid_solution
+    @grid.each do |line|
       puts line
     end
     puts @low_count
   end
-  
+
   def test_position(row, col, value)
+    # p "in test position"
     grid_store = @grid
     position_store = @position
+    print_maze
     if self.grid[row][col] == value
       @position  = [row, col]
       @counter += 1
       find_next_move if value == " "
       true
     end
+    @counter -= 1
     @grid = grid_store
     @position = position_store
-    false
-  end  
+  end
 end
